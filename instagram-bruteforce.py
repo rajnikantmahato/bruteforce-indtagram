edfbe94718677a554f872b90ed9698fa7fd221e4
rajnikantmahato <58804492+rajnikantmahato@users.noreply.github.com>
2020-10-09 05:24:46 +0300

    Add files via upload

---
+++ instagram-bruteforce.py
@@ -0,0 +1,20 @@
+import hashlib
+import sys
+from os import system, path
+password = "47629dd758de8b276b39c2a135df6682"
+
+while True:
+	print ("made By Rajni")
+	print ("Type Ctrl^ c to exit")
+	print ("Or Type exnple press Ctrl key or C key to exit")
+	input = raw_input("Enter your password: ")
+	if hashlib.md5(input).hexdigest() == password:
+		print ("welcome to the facebook Hack")
+		print ("made By Rajni")
+                system('python3 V.py')		
+
+else:
+	print ("Wrong Password")
+	print  ("Conntect To 9122592259")
+        
+	
